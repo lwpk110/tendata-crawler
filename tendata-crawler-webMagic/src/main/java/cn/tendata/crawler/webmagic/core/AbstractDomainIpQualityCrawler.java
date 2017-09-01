b1396aa30e1d5137e608cd1802712101aefb4f66
lw <lwpk110@163.com>
2017-09-01 10:22:59 +0800

    删除部分冗余代码

--- tendata-crawler-webMagic/src/main/java/cn/tendata/crawler/webmagic/core/AbstractDomainIpQualityCrawler.java
+++ tendata-crawler-webMagic/src/main/java/cn/tendata/crawler/webmagic/core/AbstractDomainIpQualityCrawler.java
@@ -1,16 +1,24 @@
 package cn.tendata.crawler.webmagic.core;
 
+import cn.tendata.crawler.webmagic.Item.processror.multirbl.MultiblCrawlerPipeline;
+import cn.tendata.crawler.webmagic.Item.processror.talos.TalosCrawlerPipeline;
+import cn.xinbee.rcs.data.domain.MailAgentDomain;
+
 /**
  * {@inheritDoc}
  *
  * Created by ernest on 2017/8/24.
  */
-public class AbstractDomainIpQualityCrawler {
+public abstract class AbstractDomainIpQualityCrawler {
     public static String MULTIBL_KEY = "MULTI_BL";
     public static String TALOS_KEY = "TALOS";
 
     public static String BLACKLIST_SUMMARY= "blacklist_summary";
     public static String EMAIL_REPUTATION= "email_reputation";
     public static String LAST_DAY_SPAM_LEVEL= "last_day_spam_level";
+
+    abstract MultiblCrawlerPipeline runMultibl(MailAgentDomain mailAgentDomain);
+
+    abstract TalosCrawlerPipeline runTalos(MailAgentDomain mailAgentDomain);
 
 }
